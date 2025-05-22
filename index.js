1c1bcf1ddfdeb40992a6e1d6ef7b8792c72a41ea
anil2917 <vsamrat274@gmail.com>
2025-05-22 16:48:13 +0530

    up

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-express = require('express')
-const app = express()
-const cors = require("cors")
-const { json } = require('express')
+express = require("express");
+const app = express();
+const cors = require("cors");
+const { json } = require("express");
 
 const corsOptions = {
   origin: ["https://croma-frontend.vercel.app", "http://localhost:5173"],
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
-app.use(cors(corsOptions))
-app.use(express.json())
-const mongoose = require('mongoose');
-main().catch(err => console.log(err));
-
+app.use(cors(corsOptions));
+app.use(express.json());
+const mongoose = require("mongoose");
+main().catch((err) => console.log(err));
 
 // y5RyhCvsgZRG7g8S
 async function main() {
-
-  await mongoose.connect('mongodb+srv://vsamrat274:y5RyhCvsgZRG7g8S@cluster0.hhezy3r.mongodb.net/cROMA?retryWrites=true&w=majority&appName=Cluster0');
-  console.log("DATABASE CONNECT")
+  await mongoose.connect(
+    "mongodb+srv://vsamrat274:y5RyhCvsgZRG7g8S@cluster0.hhezy3r.mongodb.net/cROMA?retryWrites=true&w=majority&appName=Cluster0"
+  );
+  console.log("DATABASE CONNECT");
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 const userSchema = new mongoose.Schema({
@@ -33,15 +33,17 @@ const userSchema = new mongoose.Schema({
   gender: String,
   dob: String,
   doa: String,
-
-})
-const user = mongoose.model('user', userSchema);
+});
+const user = mongoose.model("user", userSchema);
 
 app.post("/creatuser", async (req, res) => {
-
-  const findemail = await user.findOne({ email: req.body.email })
+  const findemail = await user.findOne({ email: req.body.email });
   if (findemail) {
-    res.json({ data: findemail, status: true, message: "Email is already registered" })
+    res.json({
+      data: findemail,
+      status: true,
+      message: "Email is already registered",
+    });
   } else {
     const newuser = await new user({
       mobile: req.body.mobile,
@@ -54,43 +56,43 @@ app.post("/creatuser", async (req, res) => {
       gender: req.body.gender,
       dob: req.body.dob,
       doa: req.body.doa,
-
     });
-    const saveuser = await newuser.save()
-    res.json({data : saveuser ,message:"newemail" })
+    const saveuser = await newuser.save();
+    res.json({ data: saveuser, message: "newemail" });
   }
-})
+});
 
 app.post("/Profile", async (req, res) => {
   //  const findAAAA2 = await user.findOne({ mobile: req.body.number })
-  const updateuser = await user.findOneAndUpdate({ mobile: req.body.number }, req.body,
-    { new: true })
-  res.json({ status: true, data: updateuser })
-})
+  const updateuser = await user.findOneAndUpdate(
+    { mobile: req.body.number },
+    req.body,
+    { new: true }
+  );
+  res.json({ status: true, data: req.body });
+});
 
 app.post("/allUser", async (req, res) => {
-  let contact = req.body.contact
+  let contact = req.body.contact;
   try {
-    const finduser = await user.findOne({ mobile: contact })
+    const finduser = await user.findOne({ mobile: contact });
     if (finduser) {
-      res.json({ data: finduser, status: true })
+      res.json({ data: finduser, status: true });
     } else {
-      res.json({ data: finduser, status: false })
+      res.json({ data: finduser, status: false });
     }
   } catch (error) {
-    res.json(error)
+    res.json(error);
   }
-})
-
+});
 
 app.get("/", (req, res) => {
   res.json({
     message: "Api run",
     status: true,
-  })
-})
-
+  });
+});
 
 app.listen(8080, () => {
-  console.log("server run")
-})
+  console.log("server run");
+});
